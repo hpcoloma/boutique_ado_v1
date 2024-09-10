a69fb78f5ce38a71f7f188802cbca9ec477e3476
holly coloma <holly.life.15@gmail.com>
2024-09-10 21:38:17 +0000

    Update ALLOWED_HOSTS for Heroku

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['boutique-ado-by-hc.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['boutique-ado-by-hc-eaa564404ad2.herokuapp.com', 'localhost', '127.0.0.1']
+
 
 
 # Application definition
