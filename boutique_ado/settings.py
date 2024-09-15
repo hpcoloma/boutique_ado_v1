376e327beabd4d1b9987ad338aaf4dbb59b65ffc
holly coloma <holly.life.15@gmail.com>
2024-09-15 21:53:07 +0000

    Update settings, set debug to os environment

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -24,11 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', ' ')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['boutique-ado-by-hc-eaa564404ad2.herokuapp.com', 'localhost', '127.0.0.1']
-
-
 
 # Application definition
 
